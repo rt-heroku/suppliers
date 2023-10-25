f0c705dc76512ff2618911650b2908554ea3035b
Rodrigo Torres <rtorres@rodrigos-mbp.lan>
2023-10-25 13:22:58 -0400

    Changes made: 25/10/23 13:22:58

--- src/main/java/mx/towers/suppliers/controllers/CustomerController.java
+++ src/main/java/mx/towers/suppliers/controllers/CustomerController.java
@@ -6,6 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import mx.towers.suppliers.services.AccountService;
@@ -54,4 +55,20 @@ public class CustomerController {
         return theme.getPageView("customers", "list");
     }
 
+    @GetMapping("/view/{id}")
+    public String viewCustomers(@PathVariable Integer id, Model model) {
+        model.addAttribute("accounts", accountService.getAccountById(id));
+        model.addAttribute("theme", theme);
+        model.addAttribute("settings", settings);
+        return theme.getPageView("customers", "view");
+    }
+    
+    @GetMapping("/getting-started")
+    public String gettingStarted(Model model) {
+        model.addAttribute("theme", theme);
+        model.addAttribute("settings", settings);
+        return theme.getPageView("customers", "getting-started");
+    }
+    
+    
 }
